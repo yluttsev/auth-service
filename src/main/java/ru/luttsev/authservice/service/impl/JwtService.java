0e9a80a69f5093cb0915516e9a78ac6f46e31278
Yuri Luttsev <floomyz@vk.com>
2024-07-27 17:52:56 +0300

    feat(service): добавлен сервис для работы с JWT токенами

---
+++ src/main/java/ru/luttsev/authservice/service/impl/JwtService.java
@@ -0,0 +1,63 @@
+package ru.luttsev.authservice.service.impl;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.access.secret}")
+    private String accessSecret;
+
+    @Value("${jwt.refresh.secret}")
+    private String refreshSecret;
+
+    @Value("${jwt.access.expiration}")
+    private long accessExpirationTime;
+
+    @Value("${jwt.refresh.expiration}")
+    private long refreshExpirationTime;
+
+    public String generateAccessToken(String username) {
+        return JWT.create()
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(new Date().toInstant().plus(accessExpirationTime, ChronoUnit.MILLIS))
+                .withSubject(username)
+                .sign(Algorithm.HMAC256(accessSecret));
+    }
+
+    public String generateRefreshToken(String username) {
+        return JWT.create()
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date().toInstant().plus(refreshExpirationTime, ChronoUnit.MILLIS))
+                .withSubject(username)
+                .sign(Algorithm.HMAC256(refreshSecret));
+    }
+
+    public Date getTokenExpiration(String token) {
+        return JWT.decode(token)
+                .getExpiresAt();
+    }
+
+    public boolean validateAccessToken(String token) {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(accessSecret)).build();
+        try {
+            verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromAccessToken(String token) {
+        return JWT.decode(token).getSubject();
+    }
+
+}

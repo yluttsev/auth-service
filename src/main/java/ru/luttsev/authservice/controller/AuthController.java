72789e159ca9015cae565676c3559aa46f4a3496
Yuri Luttsev <floomyz@vk.com>
2024-07-27 17:57:09 +0300

    feat(controller): добавлен функционал в AuthController

--- src/main/java/ru/luttsev/authservice/controller/AuthController.java
+++ src/main/java/ru/luttsev/authservice/controller/AuthController.java
@@ -1,27 +1,42 @@
 package ru.luttsev.authservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.luttsev.authservice.model.payload.SignInRequest;
-import ru.luttsev.authservice.model.payload.SignInResponse;
-import ru.luttsev.authservice.model.payload.SignUpRequest;
+import ru.luttsev.authservice.model.payload.request.SignInRequest;
+import ru.luttsev.authservice.model.payload.response.SignInResponse;
+import ru.luttsev.authservice.model.payload.request.SignUpRequest;
+import ru.luttsev.authservice.model.payload.response.SignUpResponse;
+import ru.luttsev.authservice.model.payload.response.TokenRefreshResponse;
+import ru.luttsev.authservice.service.AuthService;
+
+import javax.security.auth.login.CredentialException;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PutMapping("/signup")
-    public ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest) {
-        return null;
+    public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest) {
+        return authService.signUp(signUpRequest);
     }
 
     @PostMapping("/signin")
-    public SignInResponse signIn(@RequestBody SignInRequest signInRequest) {
-        return null;
+    public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest signInRequest) throws CredentialException {
+        return authService.signIn(signInRequest);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<TokenRefreshResponse> refreshToken(HttpServletRequest request) {
+        return authService.refreshToken(request);
     }
 
 }

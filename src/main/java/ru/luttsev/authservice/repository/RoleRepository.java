3386d7551fc37475e1c341019dff168bc0692e96
Yuri Luttsev <floomyz@vk.com>
2024-07-28 14:47:19 +0300

    refactor(repository): поправлен запрос получения ролей пользователя

--- src/main/java/ru/luttsev/authservice/repository/RoleRepository.java
+++ src/main/java/ru/luttsev/authservice/repository/RoleRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.luttsev.authservice.model.entity.Role;
 
+import java.util.Set;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
 
-    @Query("select a_u.role from AppUser a_u where a_u.login = :login")
-    Role findRolesByLogin(@Param("login") String login);
+    @Query("select a_u.roles from AppUser a_u where a_u.login = :login")
+    Set<Role> findRolesByLogin(@Param("login") String login);
 
 }

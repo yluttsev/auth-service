3238a719564b262b465377f66da2e703e1a8247d
Yuri Luttsev <floomyz@vk.com>
2024-07-28 15:39:30 +0300

    refactor(security): включена защита методов в конфигурации Spring Security

--- src/main/java/ru/luttsev/authservice/config/SecurityConfig.java
+++ src/main/java/ru/luttsev/authservice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -20,6 +21,7 @@ import ru.luttsev.authservice.service.impl.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 

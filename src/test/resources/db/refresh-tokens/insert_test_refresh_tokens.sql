4ef1ff5fe448259a83dd17dc6700d49bdb8663c6
Yuri Luttsev <floomyz@vk.com>
2024-07-30 13:32:04 +0300

    test: добавлены тестовые sql скрипты

---
+++ src/test/resources/db/refresh-tokens/insert_test_refresh_tokens.sql
@@ -0,0 +1,9 @@
+insert into refresh_token (id, token, user_id, expiration_date)
+values ('f7f9e04a-df88-4f2f-b63b-1e122d51073a',
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE3MjIyNzcyODIsImV4cCI6MTc1MzgxMzI4MywiYXVkIjoiIiwic3ViIjoidXNlcjEifQ.4lsSROzX1otFLy5CHzNdRPA6oDsWNUE8pVU9KBaBa4Y',
+        'dbb08c45-4789-4919-a765-54c30f54996a',
+        '2025-07-29 18:21:23.477'),
+       ('6eb1b9f1-f8f3-46d2-947b-2575013acbf6',
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE3MjIyNzczNDEsImV4cCI6MTc1MzgxMzM0MiwiYXVkIjoiIiwic3ViIjoidXNlcjIifQ.noNt3UIfSiE82VKnxNiBIW3GUzhG5eUbGxe6emVuX3g',
+        '339189f9-a9a7-4452-b374-b75a8ebbb363',
+        '2025-07-29 18:22:22.142');
